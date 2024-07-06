45d397e2811f50caea21ea132a2a747fedd375e5
swetha2200031729 <swevara@gmail.com>
2024-07-06 18:53:24 +0530

    Initial Project Setup

---
+++ src/main/java/EP/Grievance_Managment_EP/App.java
@@ -0,0 +1,13 @@
+package EP.Grievance_Managment_EP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
